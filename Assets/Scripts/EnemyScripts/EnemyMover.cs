113229b86cb1f26de5a0aad70450d03a744f1477
Ceclik <yura11122345@gmail.com>
2023-10-08 20:56:16 +0300

    Some bugs fixed

--- Assets/Scripts/EnemyScripts/EnemyMover.cs
+++ Assets/Scripts/EnemyScripts/EnemyMover.cs
@@ -3,6 +3,13 @@ using UnityEngine;
 public class EnemyMover : MonoBehaviour
 {
    [SerializeField] private float movingSpeed;
+   private GameObject _enemiesParent;
+
+   private void Awake()
+   {
+       _enemiesParent = GameObject.Find("Enemies");
+       transform.SetParent(_enemiesParent.transform);
+   }
 
    private void FixedUpdate()
    {
