4771c7112691843df4384fa6c496906651a62132
Ceclik <yura11122345@gmail.com>
2023-10-13 00:28:22 +0300

    Added error message when creating nameless room

--- Assets/Scripts/ConnectionScripts/RoomCreator.cs
+++ Assets/Scripts/ConnectionScripts/RoomCreator.cs
@@ -6,14 +6,32 @@ using UnityEngine.UI;
 public class RoomCreator : MonoBehaviourPunCallbacks
 {
    [SerializeField] private InputField roomNameField;
+   [SerializeField] private Color targetColorToWrongTextField;
+   [SerializeField] private Text errorText;
+   private bool _isErrorShown = false;
 
    public void OnCreateButtonClick()
    {
-      RoomOptions roomOptions = new RoomOptions();
-      roomOptions.MaxPlayers = 4;
-      PhotonNetwork.CreateRoom(roomNameField.text, roomOptions);
+      if (roomNameField.text.Length > 2)
+      {
+         if (_isErrorShown)
+         {
+            _isErrorShown = false;
+            roomNameField.GetComponent<Image>().color = Color.white;
+            errorText.enabled = false;
+         }
+         RoomOptions roomOptions = new RoomOptions();
+         roomOptions.MaxPlayers = 4;
+         PhotonNetwork.CreateRoom(roomNameField.text, roomOptions);
+      }
+      else
+      {
+         _isErrorShown = true;
+         roomNameField.GetComponent<Image>().color = targetColorToWrongTextField;
+         errorText.enabled = true;
+      }
    }
-
+   
    public override void OnCreatedRoom()
    {
       PhotonNetwork.LoadLevel("Gameplay");  
